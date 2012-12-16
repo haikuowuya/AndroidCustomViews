adf614598651940747fdc8e5b85eceaf74e913f7
Michael Pardo <pardom@mobiata.com>
2012-12-16 13:46:23 -0500

    Add MarqueeTextView.

--- src/com/michaelpardo/androidcustomviews/MainActivity.java
+++ src/com/michaelpardo/androidcustomviews/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,9 +13,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
 }
---
+++ src/com/michaelpardo/androidcustomviews/widget/MarqueeTextView.java
@@ -0,0 +1,25 @@
+package com.michaelpardo.androidcustomviews.widget;
+
+import android.content.Context;
+import android.text.TextUtils.TruncateAt;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class MarqueeTextView extends TextView {
+	public MarqueeTextView(Context context) {
+		super(context);
+	}
+
+	public MarqueeTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	public boolean isFocused() {
+		if (getEllipsize() == TruncateAt.MARQUEE) {
+			return true;
+		}
+
+		return super.isFocused();
+	}
+}
