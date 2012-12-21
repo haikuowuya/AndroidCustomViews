9eeaf3c22e8020bbfbc6f403dabcd80df60b246b
Michael Pardo <pardom@mobiata.com>
2012-12-21 10:53:08 -0500

    Remove unused menu.

--- src/com/michaelpardo/androidcustomviews/MainActivity.java
+++ src/com/michaelpardo/androidcustomviews/MainActivity.java
@@ -1,19 +1,12 @@
 package com.michaelpardo.androidcustomviews;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
 	}
 }
